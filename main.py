b210745b92ce71726baf828e72a0f3d9e636cecd
J3n3ns <stoneisfun@gmail.com>
2021-12-02 17:00:32 +0800

    asd

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+import re
 
 app = Flask(__name__)
+
+def check_content(query) -> list:
+    regex = r'''^[A-Za-z0-9 ]+$'''
+    temp_data = str(query).strip()
+
+    ptn = re.compile(regex)
+
+    if not bool(ptn.search(temp_data)):
+        return False
+
+    else:
+        return temp_data
 
 @app.route("/")
 def home():
@@ -9,7 +22,10 @@ def home():
 @app.route("/search", methods=['POST'])
 def result():
     query = request.form['query']
-    return render_template("result.html", query=query)
+    if check_content(query):
+        return render_template("result.html", query=query)
+    else:
+        return redirect(url_for("home"))
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
