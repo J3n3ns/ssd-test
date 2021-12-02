4eb6ae2899bdea51fb7acf50620e427aadec905b
J3n3ns <stoneisfun@gmail.com>
2021-12-02 16:49:00 +0800

    asd

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return render_template('index.html', title="page")
+def home():
+    return render_template("index.html")
+
+@app.route("/search", methods=['POST'])
+def result():
+    query = request.form['query']
+    return render_template("result.html", query=query)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
